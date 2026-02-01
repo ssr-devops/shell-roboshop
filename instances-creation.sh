725de6ef495ae4ae5e16d89cfe03c87f07495d81
ssr-devops <surendar,dev08@gmail.com>
2026-02-01 12:48:25 +0530

    instance creation and update the route53 records

---
+++ instances-creation.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+SG_ID="sg-0f45cd6cf89edb6b8"
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z10009582WYV74N1YB82Q"  # Replace with your hosted zone ID
+DOMAIN_NAME="ssrdevops.online"
+
+for instance in $@
+do
+    instance_id=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[].PrivateIpAddress' \
+    --output text)
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME"  #ssrdevops.online
+    else
+        IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[].Instances[].PrivateIpAddress' \
+        --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" #service.ssrdevops.online
+    fi
+
+    echo "$instance Instance Created with IP Address: $IP"
+
+
+    #Update the route53 record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \  # Replace with your hosted zone ID
+    --change-batch '{
+        "Comment": "Creating A record for '$instance'",
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
+
+    echo "Route53 record created for $RECORD_NAME pointing to $IP"
+
+done
